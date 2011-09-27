48ecfee0cd9e899106d830ac367224a636edc83a
Blake Eggemeyer <i.linwin@gmail.com>
2011-09-27 10:20:34 -0800

    next commit: does not work

---
+++ GNU_Ballistics.cpp
@@ -0,0 +1,9 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include "InputWindow.h"
+using namespace std;
+int main (){
+   InputWindow win(400,500,"GNU Exterior Ballistics Computer");
+   return Fl::run();
+}
+
